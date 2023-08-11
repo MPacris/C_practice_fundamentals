b943a441ec5fb9cb9e0ebe34c138fb2894010808
MPacris <mpacris@yahoo.com>
2023-08-11 11:31:35 -0500

    string concatenation

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+string myFavoriteFFootballTeam = "Saints";
+//char firstLetter = myFavoriteFFootballTeam[0]
+
+string cityName = "New Orleans";
+string teamName = "Saints";
+string footballTeam = cityName + " " + teamName;
+Console.WriteLine(footballTeam);
