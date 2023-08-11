229172b63a6b1667e2edeea21ab29d0f8b94404d
MPacris <mpacris@yahoo.com>
2023-08-11 11:57:37 -0500

    boolean if then

--- Program.cs
+++ Program.cs
@@ -2,10 +2,33 @@
 Console.WriteLine("Hello, World!");
 
 
-string myFavoriteFFootballTeam = "Saints";
-//char firstLetter = myFavoriteFFootballTeam[0]
-
+//Strings and Concatenation
 string cityName = "New Orleans";
 string teamName = "Saints";
 string footballTeam = cityName + " " + teamName;
 Console.WriteLine(footballTeam);
+
+//bool flow control Demo
+bool CanRideTheRollerCoaster(int heightInches, double weightPounds)
+{
+    int heightRequirement = 60;
+    int weightRequirement = 100;
+
+    if (heightInches >= heightRequirement && weightPounds >= weightRequirement)
+    {
+        Console.WriteLine("Ride the Ride!!!");
+        return true;
+    }
+    else if (heightInches >= heightRequirement && weightRequirement < weightPounds)
+    {
+        Console.WriteLine("You meet the height requirement but not the weight");
+        return false;
+    }
+    else
+    {
+        Console.WriteLine("No ride for you");
+        return false;
+    }
+}
+
+CanRideTheRollerCoaster(60,100);
